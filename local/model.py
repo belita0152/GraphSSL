3163e9557941cd1bde3a31c1935bd0fb5875117f
belita0152 <108735429+belita0152@users.noreply.github.com>
2025-12-12 23:27:38 +0900

    Implement GCN model in model.py

---
+++ local/model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, global_mean_pool
+
+class GCN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, dropout_rate=0.5):
+        super(GCN, self).__init__()
+
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.fc = nn.Linear(hidden_channels, out_channels)
+
+        self.dropout_rate = dropout_rate
+
+    def forward(self, batch):
+        x, edge_index, edge_weight, index = batch.x, batch.edge_index, batch.edge_attr, batch.batch
+
+        x = self.conv1(x, edge_index, edge_weight=edge_weight)
+        x = F.relu(x)
+        x = F.dropout(x, p=self.dropout_rate)
+
+        x = global_mean_pool(x, index)
+
+        x = self.fc(x)
+
+        return x
